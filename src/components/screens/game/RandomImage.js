e65edf6a4ccf89d19386aac0b7f8e10e8b58a4c0
laksun <ylaksun@yahoo.com>
2019-04-19 19:06:50 +0100

    game of thrones

--- src/components/screens/game/RandomImage.js
+++ src/components/screens/game/RandomImage.js
@@ -14,5 +14,115 @@ export default RandomImage = [
     {
         name: "IMAGEX",
         url: require('../../../../images/image2.jpg')
+    },
+    {
+        name: "PETER",
+        url: require('../../../../images/GOT_PeterDinklage.jpg')
+    },
+    {
+        name: "LENA",
+        url: require('../../../../images/GOT_Lena_Headey.jpg')
+    },
+    {
+        name: "EMILIA",
+        url: require('../../../../images/GOT_Emilia_Clarke.jpg')
+    },
+    {
+        name: "KIT",
+        url: require('../../../../images/GOT_Kit_Harington.jpg')
+    },
+    {
+        name: "SOPHIE",
+        url: require('../../../../images/GOT_Sophie_Turner.jpg')
+    },
+    {
+        name: "MAISIE",
+        url: require('../../../../images/GOT_Maisie_Williams.jpg')
+    },
+    {
+        name: "NIKOLAJ",
+        url: require('../../../../images/GOT_Nikolaj_Coster_Waldau.jpg')
+    },
+    {
+        name: "IAIN",
+        url: require('../../../../images/GOT_Iain_Glen.jpg')
+    },
+    {
+        name: "ALFIE",
+        url: require('../../../../images/GOT_Alfie_Allen.jpg')
+    },
+    {
+        name: "JOHN",
+        url: require('../../../../images/GOT_John_Bradley.jpg')
+    },
+    {
+        name: "CONLETH",
+        url: require('../../../../images/GOT_Conleth_Hill.jpg')
+    },
+    {
+        name: "AIDAN",
+        url: require('../../../../images/GOT_Aidan_Gillen.jpg')
+    },
+    {
+        name: "ISAAC",
+        url: require('../../../../images/GOT_Isaac_Hempstead.jpg')
+    },
+    {
+        name: "GWENDOLINE",
+        url: require('../../../../images/GOT_Gwendoline_Christie.jpg')
+    },
+    {
+        name: "LIAM",
+        url: require('../../../../images/GOT_Liam_Cunningham.jpg')
+    },
+    {
+        name: "JEROME",
+        url: require('../../../../images/GOT_Jerome_Flynn.jpg')
+    },
+    {
+        name: "NATHALIE",
+        url: require('../../../../images/GOT_Nathalie_Emmanuel.jpg')
+    },
+    {
+        name: "RORY",
+        url: require('../../../../images/GOT_Rory_Mccann.jpg')
+    },
+    {
+        name: "JULIAN",
+        url: require('../../../../images/GOT_Julian_Glover.jpg')
+    },
+    {
+        name: "BEN",
+        url: require('../../../../images/GOT_Ben_Crompton.jpg')
+    },
+    {
+        name: "CARICE",
+        url: require('../../../../images/GOT_Carice_Van_Houten.jpg')
+    },
+    {
+        name: "NATALIE",
+        url: require('../../../../images/GOT_Natalie_Dormer.jpg')
+    },
+    {
+        name: "MICHELLE",
+        url: require('../../../../images/GOT_Michelle_Fairley.jpg')
+    },
+    {
+        name: "HANNAH",
+        url: require('../../../../images/GOT_Hannah_Murray.jpg')
+    },
+    {
+        name: "SIBEL",
+        url: require('../../../../images/GOT_Sibel_Kekilli.jpg')
+    },
+    {
+        name: "DIANA",
+        url: require('../../../../images/GOT_Diana_Rigg.jpg')
+    },
+    {
+        name: "NATHALIA",
+        url: require('../../../../images/GOT_Nathalia_Tena.jpg')
     }
+
+
 ]
